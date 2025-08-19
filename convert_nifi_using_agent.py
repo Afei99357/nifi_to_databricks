21a707b0131861655702359e97e368aad4feb63a
Afei99357 <Afei99357>
2025-08-19 19:36:09 +0000

    new changes

--- convert_nifi_using_agent.py
+++ convert_nifi_using_agent.py
@@ -22,7 +22,6 @@ reg.seed_from_file("/Workspace/Users/eliao@bpcs.com/Agent_for_migrate_nifi_to_da
 # COMMAND ----------
 
 # MAGIC %pip install -U -qqqq backoff databricks-langchain langgraph==0.5.3 uv databricks-agents mlflow-skinny[databricks]
-# MAGIC dbutils.library.restartPython()
 
 # COMMAND ----------
 
@@ -43,11 +42,11 @@ req = ResponsesAgentRequest(input=[{
     "role": "user",
     "content": (
         "Run orchestrate_nifi_migration with:\n"
-        "xml_path=/Volumes/eliao/nifi_to_databricks/nifi_files/ICN8_BRS_Feedback.xml\n"
+        "xml_path=/Volumes/eliao/nifi_to_databricks/nifi_files/nifi_pipeline_eric_embed_groups.xml\n"
         "out_dir=/Workspace/Users/eliao@bpcs.com/Agent_for_migrate_nifi_to_databricks/output_results\n"
-        "project=nifi2dbx_test_2_feedback\n"
-        "job=job_test_feedback\n"
-        "notebook_path=/Workspace/Users/eliao@bpcs.com/Agent_for_migrate_nifi_to_databricks/output_results/nifi2dbx_test_2_feedback/main\n"
+        "project=nifi2dbx_test_1\n"
+        "job=job_test_1\n"
+        "notebook_path=/Workspace/Users/eliao@bpcs.com/Agent_for_migrate_nifi_to_databricks/output_results/nifi2dbx_test_1/main\n"
         "existing_cluster_id=0722-181403-vd3u4c6r\n"
         "deploy=true"
     )
@@ -62,47 +61,3 @@ for item in resp.output:
             if block["type"] == "output_text":
                 print(block["text"])  # list of output items; the tool's JSON will be in here
 
-
-# COMMAND ----------
-
-import xml.etree.ElementTree as ET
-
-try:
-    ET.parse("/Volumes/eliao/nifi_to_databricks/nifi_files/ICN8_BRS_Feedback.xml")
-    print("✅ XML is well-formed")
-except ET.ParseError as e:
-    print("❌ XML parsing failed:", e)
-
-
-# COMMAND ----------
-
-from utils.xml_preprocess import summarize_nifi_template
-
-xml_path = "/Volumes/eliao/nifi_to_databricks/nifi_files/ICN8_BRS_Feedback.xml"
-summary = summarize_nifi_template(xml_path)
-print(summary)
-
-
-# COMMAND ----------
-
-from mlflow.types.responses import ResponsesAgentRequest
-from agents import AGENT
-
-req = ResponsesAgentRequest(input=[{
-    "role": "user",
-    "content": (
-        f"Run orchestrate_nifi_migration using this summarized NiFi XML:\n"
-        f"{summary}\n"
-        "out_dir=/Workspace/Users/eliao@bpcs.com/Agent_for_migrate_nifi_to_databricks/output_results\n"
-        "project=nifi2dbx_feedback\n"
-        "job=job_feedback\n"
-        "deploy=true"
-    )
-}])
-
-resp = AGENT.predict(req)
-for item in resp.output:
-    if item.type == "message":
-        for block in item.content:
-            if block["type"] == "output_text":
-                print(block["text"])
