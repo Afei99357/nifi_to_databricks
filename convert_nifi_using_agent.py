01f1cdc15b151d6c5518cf11416e40a8d70929b7
Afei99357 <Afei99357>
2025-08-20 00:16:39 +0000

    update the code to use the agent

--- convert_nifi_using_agent.py
+++ convert_nifi_using_agent.py
@@ -29,18 +29,29 @@ dbutils.library.restartPython()
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC Simpler file
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Complex NIFI Workflow
+
+# COMMAND ----------
+
 from mlflow.types.responses import ResponsesAgentRequest
 from agents import AGENT
 
 req = ResponsesAgentRequest(input=[{
     "role": "user",
     "content": (
-        "Run orchestrate_nifi_migration with:\n"
+        "Run orchestrate_chunked_nifi_migration with:\n"
         "xml_path=/Volumes/eliao/nifi_to_databricks/nifi_files/nifi_pipeline_eric_embed_groups.xml\n"
-        "out_dir=/Workspace/Users/eliao@bpcs.com/nifi_to_databricks/output_results\n"
-        "project=nifi2dbx_test_1\n"
-        "job=job_test_1\n"
-        "notebook_path=/Workspace/Users/eliao@bpcs.com/nifi_to_databricks/output_results/nifi2dbx_test_1/main\n"
+        "out_dir=/Workspace/Users/eliao@bpcs.com/nifi_to_databricks_large_xml/output_results\n"
+        "project=nifi2dbx_group_test\n"
+        "job=job_test_group_test\n"
+        "notebook_path=/Workspace/Users/eliao@bpcs.com/nifi_to_databricks_large_xml/output_results/nifi2dbx_group_test/main\n"
+        "max_processors_per_chunk=25\n"
         "existing_cluster_id=0722-181403-vd3u4c6r\n"
         "deploy=true"
     )
@@ -48,10 +59,41 @@ req = ResponsesAgentRequest(input=[{
 
 resp = AGENT.predict(req)
 
-# Only print text outputs
 for item in resp.output:
     if item.type == "message":
         for block in item.content:
             if block["type"] == "output_text":
-                print(block["text"])  # list of output items; the tool's JSON will be in here
+                print(block["text"])
 
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Complex NIFI Workflow
+
+# COMMAND ----------
+
+from mlflow.types.responses import ResponsesAgentRequest
+from agents import AGENT
+
+req = ResponsesAgentRequest(input=[{
+    "role": "user",
+    "content": (
+        "Run orchestrate_chunked_nifi_migration with:\n"
+        "xml_path=/Volumes/eliao/nifi_to_databricks/nifi_files/ICN8_BRS_Feedback.xml\n"
+        "out_dir=/Workspace/Users/eliao@bpcs.com/nifi_to_databricks_large_xml/output_results\n"
+        "project=nifi2dbx_feedback\n"
+        "job=job_test_feedback\n"
+        "notebook_path=/Workspace/Users/eliao@bpcs.com/nifi_to_databricks_large_xml/output_results/nifi2dbx_feedback/main\n"
+        "max_processors_per_chunk=25\n"
+        "existing_cluster_id=0722-181403-vd3u4c6r\n"
+        "deploy=true"
+    )
+}])
+
+resp = AGENT.predict(req)
+
+for item in resp.output:
+    if item.type == "message":
+        for block in item.content:
+            if block["type"] == "output_text":
+                print(block["text"])
